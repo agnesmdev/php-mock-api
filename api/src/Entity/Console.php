cc13c634f1a5e17479770b78853fe5494d8ca719
agnesmdev <agnesm.dev@gmail.com>
2021-11-10 15:07:09 +0100

    [ADD] Ajout de la table console

---
+++ api/src/Entity/Console.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Context;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity]
+#[ApiResource]
+class Console
+{
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(unique: true)]
+    #[Assert\NotNull]
+    #[Assert\NotBlank]
+    public string $name;
+
+    #[ORM\Column(type: 'date')]
+    #[Context([DateTimeNormalizer::FORMAT_KEY => 'Y-m-d'])]
+    public ?\DateTimeInterface $launchDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
